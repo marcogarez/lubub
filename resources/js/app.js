6560a1b0cb202ace855f090847b5dfc0d882720e
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-01 20:44:36 -0500

    Dropzone added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import Dropzone from "dropzone";
+
+Dropzone.autoDiscover = false;
+
+if (document.getElementById('dropzone')) {
+    const dropzone = new Dropzone('#dropzone', {
+        dictDefaultMessage: 'Sube tu imagen',
+        acceptedFiles: '.png, .jpg, .jpeg, .gif',
+        addRemoveLinks: true,
+        dictRemoveFile: 'Borrar archivo',
+        maxFiles: 1,
+        uploadMultiple: false,
+    });
+}
