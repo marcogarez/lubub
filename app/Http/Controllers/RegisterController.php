a10eb389e9a1b3ea806a2b4c1afc75d20a9fe4e8
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-01 19:24:39 -0500

    Auth added when you register a new user, auth middleware added on posts, login view added

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -32,5 +32,8 @@ class RegisterController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        auth()->attempt($request->only('email', 'password'));
+
+        return redirect()->route('posts.index');
     }
 }
