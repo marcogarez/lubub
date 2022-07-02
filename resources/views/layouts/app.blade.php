d166da3ba3d5fcd033a29a73a83b8693dd53509d
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-01 19:43:03 -0500

    Secutiry improved on logout with post method and csrf token

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,12 @@
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold text-gray-600 text-sm" href="">Hola: <span
                             class="font-normal">{{ auth()->user()->username }}</span></a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">Cerrar sesión</a>
+
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+
+                        <button class="font-bold uppercase text-gray-600 text-sm" type="submit">Cerrar sesión</button>
+                    </form>
                 </nav>
             @endauth
 
