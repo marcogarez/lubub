4ac27681ed25faff6a810e4984f3bac081779f39
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-01 19:40:41 -0500

    Logout added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold text-gray-600 text-sm" href="">Hola: <span
                             class="font-normal">{{ auth()->user()->username }}</span></a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Cerrar sesión</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">Cerrar sesión</a>
                 </nav>
             @endauth
 
