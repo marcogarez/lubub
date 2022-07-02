6835d322ed3acda241c7769a5947a575594a32b3
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-01 20:01:16 -0500

    Remember me token functionallity added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,6 +36,10 @@
                     @enderror
                 </div>
 
+                <div class="mb-5">
+                    <input type="checkbox" name="remember"> <label class=" text-gray-500 text-sm" for="remember">Mantener mi sesión abierta</label>
+                </div>
+
                 <input type="submit" value="Iniciar sesión"
                     class="bg-sky-600 hover:bg-sky-700 transition-colors uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
