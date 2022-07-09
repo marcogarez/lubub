576fe012fd86276f4214d3683720afc223d63c01
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-08 23:34:26 -0500

    Posts changed to components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,5 @@
 @section('titulo', 'Página principal')
 
 @section('contenido')
-    @if ($posts->count())
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            @foreach ($posts as $post)
-                <div>
-                    <a href="{{ route('posts.show', ['user' => $post->user, 'post' => $post]) }}">
-                        <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
-                    </a>
-                </div>
-            @endforeach
-        </div>
-
-        <div class="my-10">
-            {{ $posts->links() }}
-        </div>
-    @else
-        <p class="text-center">Aún no hay posts, sigue a alguien para comenzar a visualizarlos!</p>
-    @endif
+    <x-listar-post :posts="$posts" />
 @endsection
