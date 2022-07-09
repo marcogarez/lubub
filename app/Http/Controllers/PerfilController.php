a342917e4f71fd56db27d5ae9d82cf5becb382e7
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-08 20:36:59 -0500

    Change of name and upload of image finished

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -16,8 +18,31 @@ class PerfilController extends Controller
         return view('perfil.index');
     }
 
-    public function store(User $user)
+    public function store(Request $request)
     {
+        $request->request->add(['username', Str::slug($request->username)]);
 
+        $this->validate($request, [
+            'username' => ['required', 'unique:users,username,' . auth()->user()->id, 'min:3', 'max:20', 'not_in:editar-perfil'],
+        ]);
+
+        if ($request->imagen) {
+            $imagen = $request->file('imagen');
+
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+            $imagenServidor = Image::make($imagen);
+            $imagenServidor->fit(1000, 1000);
+
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? '';
+        $usuario->save();
+
+        return redirect()->route('posts.index', $usuario->username);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="w-8/12 lg:w-6/12 px-5">
-                <img src="{{ asset('img/usuario.svg') }}" alt="Imagen usuario">
+                <img src="{{ asset('perfiles/' . $user->imagen) }}" alt="Imagen usuario">
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 md:py-10">
                 <div class="flex items-center gap-4">
